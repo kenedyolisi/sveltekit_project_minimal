91dffcabc137fff77696cb26d7f2acc788e35a9b
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2026-04-02 00:07:42 +0300

    chore: add unplugin-icons plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import devtoolsJson from "vite-plugin-devtools-json";
-import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
+// import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+  plugins: [
+    tailwindcss(),
+    icons({
+      compiler: "svelte",
+      // customCollections: { local: FileSystemIconLoader("./src/static/icons") },
+    }),
+    sveltekit(),
+    devtoolsJson(),
+  ],
 });
